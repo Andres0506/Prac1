b75e1b0fa2be5dc35c60adecbd77f056aef589ac
Andres0506 <70180815+Andres0506@users.noreply.github.com>
2021-09-20 08:58:28 -0500

    Add files via upload

---
+++ SQLDBPrac1/CURSO.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[CURSO]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [Nombre] NVARCHAR(50) NULL, 
+    [Fecha_Inicio] NVARCHAR(50) NOT NULL, 
+    [Duracion] INT NOT NULL, 
+    [Valor] INT NOT NULL
+)

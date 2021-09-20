58cc4ca369ab7367a414d0ae30f344a901c89f2a
Andres0506 <70180815+Andres0506@users.noreply.github.com>
2021-09-20 08:59:22 -0500

    Delete SQLDBPrac1 directory

--- SQLDBPrac1/CURSO.sql
+++
@@ -1,8 +0,0 @@
-﻿CREATE TABLE [dbo].[CURSO]
-(
-	[Id] INT NOT NULL PRIMARY KEY, 
-    [Nombre] NVARCHAR(50) NULL, 
-    [Fecha_Inicio] NVARCHAR(50) NOT NULL, 
-    [Duracion] INT NOT NULL, 
-    [Valor] INT NOT NULL
-)
